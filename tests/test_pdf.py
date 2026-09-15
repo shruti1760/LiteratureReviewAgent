0910bed8e2cab78cd71b41d4add1dac2ec0cb9f0
shruti1760 <nshruti760@gmail.com>
2026-09-15 22:46:32 +0530

    changed file location

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,6 +1,6 @@
 from services.pdf_loader import extract_text_from_pdf
 
 
-text = extract_text_from_pdf("papers\\294_Final.pdf")
+text = extract_text_from_pdf("papers\Impact-of-PM-and-BM-on-Success.pdf")
 
 print(text[:3000])
